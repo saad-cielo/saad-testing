6c8c72cbe9f859ee63fbf9d9c79a4d98c02277fa
saad-cielo <138462230+saad-cielo@users.noreply.github.com>
2023-08-07 14:32:35 +0500

    saad

--- function1.py
+++ function1.py
@@ -1,1 +1,1 @@
-print("saad")
+print("docker run -it -d --name server -u root -v /teamcity/data:/data/teamcity_server/datadir -v /teamcity/logs:/opt/teamcity/logs -p 8111:8111 jetbrains/teamcity-server")
